025081c96309d38be93d85cdf3a8e77d43a89219
nestazhu <nesta@QY-ABdeMacBook-Air.local>
2020-07-22 16:28:58 +0800

    2rd commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-import React,{component} from 'react'
+import React,{Component} from 'react'
 
-export default class App extends component{
+export default class App extends Component{
     render(){
         return (
             <div>App</div>
